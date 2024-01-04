038ccb31bed686f47e3afb74d2cfa236369e0579
Adam Braimbridge <adam@braimbridge.com>
2024-01-04 03:34:01 +0000

    Add Commodore64Basic component and video player

---
+++ components/Commodore64Basic.vue
@@ -0,0 +1,27 @@
+<template>
+  <video-player :options="videoOptions" />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+
+export default defineComponent({
+  components: { VideoPlayer },
+  data: () => ({
+    videoOptions: {
+      autoplay: true,
+      controls: false,
+      fluid: true,
+      sources: [
+        {
+          src: '/video/20goto10run.mp4',
+          poster: '/img/20goto10run.jpg',
+          type: 'video/mp4',
+        }
+      ]
+    }
+  })
+})
+</script>
