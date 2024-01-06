2af916a10da0a8837e468a629359987e940d1b2f
Adam Braimbridge <adam@braimbridge.com>
2024-01-06 07:08:24 +0000

    Update app.vue styling and assets

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex justify-center items-center bg-black">
+  <div class="h-screen flex justify-center items-center bg-[#6c57b2]">
     <Commodore64Basic />
   </div>
 </template>
