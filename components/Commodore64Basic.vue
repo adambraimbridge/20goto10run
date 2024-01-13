d6666f47c2df6141c424d93d07f868fb2540fece
Adam Braimbridge <adam@braimbridge.com>
2024-01-13 15:00:27 +0000

    Refactor Commodore64Basic.vue component layout

--- components/Commodore64Basic.vue
+++ components/Commodore64Basic.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid place-content-center p-5">
-    <img class="rounded-lg w-screen max-h-full drop-shadow-lg" src="/img/Commodore64BasicAnimated.webp" />
+  <div class="grid place-items-center p-5">
+    <img width="800" height="451" class="rounded-lg drop-shadow-lg" src="/img/Commodore64BasicAnimated.webp" />
   </div>
 </template>
 
